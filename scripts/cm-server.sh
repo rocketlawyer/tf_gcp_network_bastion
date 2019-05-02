201f73a9c60dd4978dc4e2c6b3ddf5b3c00f4397
alcher <alcher@yahoo.com>
2019-05-02 17:00:04 +0800

    remove more old files

--- scripts/cm-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "test"
-# CM Install
-#sudo apt-get install -y cloudera-manager-server cloudera-manager-server-db-2
-#sudo apt-get install -y cloudera-manager-server 
-#sudo service cloudera-scm-server-db start
-#sudo service cloudera-scm-server start
