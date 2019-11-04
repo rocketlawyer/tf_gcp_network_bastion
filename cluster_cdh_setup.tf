3d0120ca4e8f6bd7bf212cdaa423411ff61bd272
alcher <alcher@yahoo.com>
2019-11-04 17:47:23 +0800

    enabled hdfs testing

--- cluster_cdh_setup.tf
+++ cluster_cdh_setup.tf
@@ -10,11 +10,19 @@ resource "null_resource" "cdh_cluster_setup" {
         host = "${google_compute_address.bastion_public_ip.address}"
         user = "${var.admin_user}"
         private_key = "${file(var.admin_privkey_file)}"
+        bastion_host = "${google_compute_address.bastion_public_ip.address}"
+        bastion_user = "${var.admin_user}"
+        bastion_private_key = "${file(var.admin_privkey_file)}"
    }
 
   provisioner "file" {
     source      = "/tmp/hostfile.txt"
     destination = "/tmp/hostfile.txt"
+  }
+
+   provisioner "file" {
+    source      = "${var.admin_privkey_file}"
+    destination = "~/.ssh/ssh_key"
   }
 
   provisioner "remote-exec" {
@@ -25,10 +33,10 @@ resource "null_resource" "cdh_cluster_setup" {
     "sudo pip install cm-api",
     "sudo pip install --upgrade pip",
     "sudo echo -e 'StrictHostKeyChecking no\n' >> ~/.ssh/config; sudo chmod 600 ~/.ssh/config",
-#     "git clone https://github.com/alcher/cloudera-centos terraform-hadoop"
-#    "sleep 10 && cd terraform-hadoop && ansible-playbook -i hosts site.yml"
+    "sudo chmod 600 ~/.ssh/ssh_key", 
+    "git clone https://github.com/alcher/cloudera-centos terraform-hadoop",
+    "sleep 10 && cd terraform-hadoop && ansible-playbook -i hosts --private-key ~/.ssh/ssh_key site.yml",
     ]
   }
 
 }
-
